475a00ab43292d35f348f920f542d0fccb5ca983
Thiago Oliveira <thiago@outlook.com.br>
2021-04-25 13:07:42 -0300

    Initial commit with PromiseKit dependency

---
+++ PromiseKit/PromiseKitSandbox/Pods/Target Support Files/Pods-PromiseKitSandbox-PromiseKitSandboxUITests/Pods-PromiseKitSandbox-PromiseKitSandboxUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PromiseKitSandbox_PromiseKitSandboxUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PromiseKitSandbox_PromiseKitSandboxUITests
+@end
