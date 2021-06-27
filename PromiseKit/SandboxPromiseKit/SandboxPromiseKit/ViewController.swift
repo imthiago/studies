fa45805f8329e8f68456a557bb150e66a376f2c8
Thiago Oliveira <thiago@outlook.com.br>
2021-06-27 18:06:43 -0300

    Initial

---
+++ PromiseKit/SandboxPromiseKit/SandboxPromiseKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SandboxPromiseKit
+//
+//  Created by Thiago Oliveira on 27/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
